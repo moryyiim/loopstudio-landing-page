9f48d8a73bedb62903e49612f465f599adf67f07
Big Mac 7 <bigmac7@bigs-imac.home>
2023-05-26 13:14:53 -0700

    fix mobile menu

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 const hamburger = document.querySelector(".hamburger");
 const mobileMenu = document.querySelector(".mobile-menu");
+// const mobileLinks = documetn.querySelectorAll('.mobile-nav-links')
 
 hamburger.addEventListener("click", function () {
   mobileMenu.classList.toggle("active");
+
+  //   ternary operator to apply overflow-y: hidden when mobile menu is active
+  document.body.style.overflowY = mobileMenu.classList.contains("active")
+    ? "hidden"
+    : "initial";
 });
